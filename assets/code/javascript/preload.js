26ff20f1a8ef80d5898574646a5c46de6a2032cc
XZY <xzy12@vk.com>
2025-06-23 22:41:34 +0500

    Update preload.js

--- assets/code/javascript/preload.js
+++ assets/code/javascript/preload.js
@@ -1,7 +1,11 @@
-const { contextBridge, ipcRenderer } = require('electron')
+import { contextBridge, ipcRenderer } from 'electron';
 
-contextBridge.exposeInMainWorld('api', {
-  checkInternet: () => ipcRenderer.invoke('check-internet'),
-  reloadApp: () => ipcRenderer.invoke('reload-app'),
-  controlWindow: (action) => ipcRenderer.send('window-control', action)
-})
+contextBridge.exposeInMainWorld('electronAPI', {
+  getSettings: () => ipcRenderer.invoke('get-settings'),
+  setSetting: (key, value) => ipcRenderer.invoke('set-setting', key, value),
+  checkConnection: () => ipcRenderer.invoke('check-connection'),
+  onUpdateAvailable: (callback) => ipcRenderer.on('update-available', (_, version) => callback(version)),
+  onUpdateProgress: (callback) => ipcRenderer.on('update-progress', (_, data) => callback(data)),
+  onUpdateDownloaded: (callback) => ipcRenderer.on('update-downloaded', () => callback()),
+  onUpdateError: (callback) => ipcRenderer.on('update-error', (_, msg) => callback(msg))
+});
