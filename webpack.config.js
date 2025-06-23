9104372367b15c9d27c09ebed086854de4a14ac3
XZY <xzy12@vk.com>
2025-06-23 23:20:02 +0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development', // или 'production' при сборке
-  entry: './assets/code/javascript/renderer/src/index.jsx',
+  entry: './assets/code/javascript/renderer/src/App.jsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'assets/code/html/dist')
