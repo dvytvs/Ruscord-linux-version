23f4316a1d6a787bd726469c652a6304c88c4647
XZY <xzy12@vk.com>
2025-06-23 23:01:34 +0500

    Create webpack.config.js

---
+++ assets/code/javascript/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // или 'production' при сборке
+  entry: './assets/code/javascript/renderer/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'assets/code/html/dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
