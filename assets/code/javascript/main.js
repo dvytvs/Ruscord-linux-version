e4435e1124221d5acd3e6295e4127d7d32c304ae
XZY <xzy12@vk.com>
2025-06-23 22:09:33 +0500

    Update main.js

--- assets/code/javascript/main.js
+++ assets/code/javascript/main.js
@@ -1,94 +1,66 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
-const { autoUpdater } = require('electron-updater');
-const Store = require('electron-store');
+const { app, BrowserWindow, ipcMain, net } = require('electron')
+const path = require('path')
 
-const store = new Store({
-  defaults: {
-    settings: {
-      devtools: false,
-      tray: true,
-      updates: true,
-      cacheClear: false,
-      updateInterval: 3600,
-      extensions: false,
-      overlay: false
-    }
-  }
-});
+let mainWindow
 
-let splashWindow;
-let mainWindow;
-
-function createSplash() {
-  splashWindow = new BrowserWindow({
-    width: 400,
-    height: 300,
-    frame: false,
-    transparent: true,
-    alwaysOnTop: true,
-    resizable: false,
-    movable: false,
-    hasShadow: false,
-    skipTaskbar: true,
-    webPreferences: {
-      nodeIntegration: true,
-      contextIsolation: false
-    }
-  });
-  splashWindow.loadFile(path.join(__dirname, 'assets', 'code', 'html', 'splash.html'));
-}
-
-function createMainWindow() {
+function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
     minWidth: 800,
     minHeight: 600,
-    backgroundColor: '#121212',
     frame: false,
+    backgroundColor: '#121212',
+    icon: path.join(__dirname, 'assets', 'Images', 'logo.png'),
     webPreferences: {
-      preload: path.join(__dirname, 'assets', 'code', 'javascript', 'preload.js'),
+      preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
-      nodeIntegration: false
+      nodeIntegration: false,
+      enableRemoteModule: false,
     }
-  });
-  mainWindow.loadFile(path.join(__dirname, 'assets', 'code', 'html', 'index.html'));
-  ipcMain.on('window-control', (e, action) => {
-    if (action === 'minimize') mainWindow.minimize();
-    else if (action === 'maximize') mainWindow.isMaximized() ? mainWindow.unmaximize() : mainWindow.maximize();
-    else if (action === 'close') mainWindow.close();
-  });
-  ipcMain.handle('get-settings', () => store.get('settings'));
-  ipcMain.handle('set-setting', (e, key, value) => {
-    const s = store.get('settings');
-    s[key] = value;
-    store.set('settings', s);
-    return s;
-  });
+  })
+
+  mainWindow.loadFile(path.join(__dirname, 'assets', 'code', 'javascript', 'renderer', 'index.html'))
 }
 
 app.whenReady().then(() => {
-  createSplash();
-  autoUpdater.autoDownload = true;
-  autoUpdater.checkForUpdates();
-  autoUpdater.on('download-progress', (progress) => {
-    splashWindow.webContents.send('update-progress', progress);
-  });
-  autoUpdater.on('update-downloaded', () => {
-    createMainWindow();
-    splashWindow.close();
-  });
-  autoUpdater.on('update-not-available', () => {
-    createMainWindow();
-    splashWindow.close();
-  });
-  autoUpdater.on('error', () => {
-    createMainWindow();
-    splashWindow.close();
-  });
-});
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
+  if (process.platform !== 'darwin') app.quit()
+})
+
+ipcMain.handle('check-internet', () => {
+  return new Promise(resolve => {
+    const request = net.request('https://www.google.com')
+    request.on('response', () => resolve(true))
+    request.on('error', () => resolve(false))
+    request.end()
+  })
+})
+
+ipcMain.handle('reload-app', () => {
+  app.relaunch()
+  app.exit()
+})
+
+ipcMain.on('window-control', (event, action) => {
+  const win = BrowserWindow.getFocusedWindow()
+  if (!win) return
+  switch (action) {
+    case 'minimize':
+      win.minimize()
+      break
+    case 'maximize':
+      win.isMaximized() ? win.unmaximize() : win.maximize()
+      break
+    case 'close':
+      win.close()
+      break
+  }
+})
