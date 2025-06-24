235c04a4176fb48f1835f41f65d5f57fd2a4e25f
XZY <xzy12@vk.com>
2025-06-24 21:23:44 +0500

    Create mainWindow.js

---
+++ assets/code/javascript/windows/mainWindow.js
@@ -0,0 +1,13 @@
+import { BrowserWindow } from 'electron';
+
+export function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+  mainWindow.loadURL('https://app.russcord.ru');
+}
