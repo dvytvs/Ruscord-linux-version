0cb7de7ea2130831db04fbfe27597ed44fecd42b
XZY <xzy12@vk.com>
2025-06-26 18:18:49 +0500

    Delete assets/code/javascript directory

--- assets/code/javascript/preload/splash-preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  onStatusUpdate: (callback) => ipcRenderer.on('status-update', (event, data) => callback(data)),
-  onProgressUpdate: (callback) => ipcRenderer.on('progress-update', (event, data) => callback(data))
-});
