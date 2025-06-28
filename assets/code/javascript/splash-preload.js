70a92df1f6a974e1f649a36c9f6d9cca900dabfc
XZY <xzy12@vk.com>
2025-06-28 21:02:51 +0500

    Update splash-preload.js

--- assets/code/javascript/splash-preload.js
+++ assets/code/javascript/splash-preload.js
@@ -1,6 +1,6 @@
-import { contextBridge, ipcRenderer } from 'electron'
+const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
   onStatus: callback => ipcRenderer.on('splash-status', (event, text) => callback(text)),
   onProgress: callback => ipcRenderer.on('splash-progress', (event, downloaded, total) => callback(downloaded, total)),
-})
+});
