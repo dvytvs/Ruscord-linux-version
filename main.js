e229b2a760030e2194f55ff5d2dc87f086bf07c1
XZY <xzy12@vk.com>
2025-06-28 21:01:28 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-import { app, BrowserWindow, Menu, shell } from 'electron'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { showSplash } from './assets/code/javascript/splash.js'
+const { app, BrowserWindow, Menu, shell } = require('electron');
+const path = require('path');
+const { showSplash } = require('./assets/code/javascript/splash.js');
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-let splashWindow
-let mainWindow
+let splashWindow;
+let mainWindow;
 
 async function createMainWindow() {
   mainWindow = new BrowserWindow({
@@ -22,16 +18,16 @@ async function createMainWindow() {
       contextIsolation: true,
       nodeIntegration: false,
     },
-  })
+  });
 
-  mainWindow.loadURL('https://app.russcord.ru')
+  mainWindow.loadURL('https://app.russcord.ru');
 
   mainWindow.once('ready-to-show', () => {
     if (splashWindow && !splashWindow.isDestroyed()) {
-      splashWindow.close()
+      splashWindow.close();
     }
-    mainWindow.show()
-  })
+    mainWindow.show();
+  });
 
   const menuTemplate = [
     {
@@ -54,26 +50,26 @@ async function createMainWindow() {
           label: 'Связаться с поддержкой',
           accelerator: 'F1',
           click: () => {
-            shell.openExternal('https://t.me/supruscord_bot')
+            shell.openExternal('https://t.me/supruscord_bot');
           },
         },
       ],
     },
-  ]
+  ];
 
-  const menu = Menu.buildFromTemplate(menuTemplate)
-  Menu.setApplicationMenu(menu)
+  const menu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(menu);
 }
 
 app.whenReady().then(async () => {
-  splashWindow = await showSplash()
-  await createMainWindow()
-})
+  splashWindow = await showSplash();
+  await createMainWindow();
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== 'darwin') app.quit();
+});
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) createMainWindow()
-})
+  if (BrowserWindow.getAllWindows().length === 0) createMainWindow();
+});
