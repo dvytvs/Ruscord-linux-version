c1c9b36b727cb0416c7f5a0c3f1e11d527635730
XZY <xzy12@vk.com>
2025-06-30 10:12:35 +0500

    Update splash-preload.js

--- assets/code/javascript/splash-preload.js
+++ assets/code/javascript/splash-preload.js
@@ -1,6 +1,11 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
-  onStatus: callback => ipcRenderer.on('splash-status', (event, text) => callback(text)),
-  onProgress: callback => ipcRenderer.on('splash-progress', (event, downloaded, total) => callback(downloaded, total)),
+  onStatus: (callback) => ipcRenderer.on('splash-status', (event, text) => callback(text)),
+  onProgress: (callback) => ipcRenderer.on('splash-progress', (event, downloaded, total, speed) => callback(downloaded, total, speed)),
+  send: (channel, ...args) => {
+    if (channel === 'cancel-download') {
+      ipcRenderer.send(channel, ...args);
+    }
+  }
 });
